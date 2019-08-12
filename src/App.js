66f2f0083404e52492f4cce5ed768b0da476e82b
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-12 17:33:02 -0500

    working on stretch added reset button

--- src/App.js
+++ src/App.js
@@ -2,31 +2,40 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import TopRow from "./TopRow";
+
+
+
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(0)
-  const [awayScore, setAwayScore] = useState(0)
-
+   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+   const [homeScore, setHomeScore] = useState(0)
+   const [awayScore, setAwayScore] = useState(0)
+   function reset(){
+    setHomeScore(0)
+    setAwayScore(0)
+   }
+  
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
+       <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
-      
-
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{ homeScore }</div>
+          <h2 className="home__name">Lions</h2>
+          
+          
+                    
+          {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state.  */}
+          
+          <div className="home__score">{ homeScore }</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ awayScore }</div>
+          <h2 className="away__name">Tigers</h2>
+          <div className="away__score">{ awayScore }</div>
           </div>
         </div>
+        {/* <TopRow /> */}
         <BottomRow />
       </section>
       <section className="buttons">
@@ -35,6 +44,7 @@ function App() {
           <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
         </div>
+        <button className="awayButtons__touchdown" onClick={reset}>Reset</button>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
@@ -44,4 +54,5 @@ function App() {
   );
 }
 
+// export {homeScore}
 export default App;
